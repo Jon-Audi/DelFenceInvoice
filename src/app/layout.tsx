d8b2a38299049e40b7bb611e0e2cb1be1ddde8e7
Jon Audiffred <jon.audiffred@gmail.com>
2025-06-04 00:05:16 +0000

    Change the title of the app to "Delaware Fence Solutions" and include it to the top left instead of "Delaware Fence Pro"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,11 @@
+
 import type { Metadata } from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
 import { Providers } from './providers';
 
 export const metadata: Metadata = {
-  title: 'Delaware Fence Pro',
+  title: 'Delaware Fence Solutions',
   description: 'Manage estimates, orders, and invoices for Delaware Fence Solutions.',
 };
 
